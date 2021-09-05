0e941cfcaeca291b63191ef10578987cc60462ab
Igor Mayor <314238817@stud.colman.ac.il>
2021-09-05 18:31:41 +0300

    igor commit 1 update

---
+++ InternetAppProject/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetAppProject.Models
+{
+    public class Class
+    {
+        // this is igor
+        int x;
+    }
+}
+
