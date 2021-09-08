fe89cfc580f164c9a3d2727d2bc8186ecb35b349
Igor Mayor <314238817@stud.colman.ac.il>
2021-09-08 20:03:58 +0300

    Created models: Drive DriveTypes

---
+++ InternetAppProject/Models/Drive.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace InternetAppProject.Models
+{
+    public class Drive
+    {
+    public int Id { get; set; }
+
+    public int User UserId { get; set; }
+
+    public IEnumerable<Image> ImageId { get; set; }
+
+    public int Current_usage { get; set; }
+
+    public DriveType TypeId { get; set; }
+
+}
+}
---
+++ InternetAppProject/Models/DriveType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetAppProject.Models
+{
+    public class DriveType
+    {
+        public int Id { get; set; }
+
+        public int Level { get; set; }
+
+        public string Name { get; set; }
+
+        public int Max_Capacity { get; set; }
+
+        public int Price { get; set; }
+
+        public DateTime Last_change { get; set; }
+    }
+}
