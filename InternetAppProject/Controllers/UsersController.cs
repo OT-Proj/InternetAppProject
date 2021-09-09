0cd03a3b7e0cf2c631cd3e913739eb317a28dfe2
ofek tal <ofek@mail.com>
2021-09-09 18:01:11 +0300

    Small login progress

--- InternetAppProject/Controllers/UsersController.cs
+++ InternetAppProject/Controllers/UsersController.cs
@@ -7,11 +7,15 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using InternetAppProject.Data;
 using InternetAppProject.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 
 namespace InternetAppProject.Controllers
 {
     public class UsersController : Controller
     {
+        private const string User_Id = "User_Id";
         private readonly InternetAppProjectContext _context;
 
         public UsersController(InternetAppProjectContext context)
@@ -22,6 +26,12 @@ namespace InternetAppProject.Controllers
         // GET: Users
         public async Task<IActionResult> Index()
         {
+            var id = User.Claims.FirstOrDefault(c => c.Type == User_Id);
+            ViewData["name"] = "unidentified";
+            if (id != null)
+            {
+                ViewData["name"] = User.Claims.FirstOrDefault(c => c.Type == User_Id).Value;
+            }
             return View(await _context.User.ToListAsync());
         }
 
@@ -61,6 +71,7 @@ namespace InternetAppProject.Controllers
                 user.Create_time = DateTime.Now;
                 _context.Add(user);
                 await _context.SaveChangesAsync();
+                LoginUser(user.Id, user.Name);
                 return RedirectToAction(nameof(Index));
             }
             return View(user);
@@ -98,6 +109,7 @@ namespace InternetAppProject.Controllers
             {
                 try
                 {
+                    // fetch old user data to keep the create_time correct
                     var existing_Data = _context.User.Where(U => U.Id == id).First();
                     user.Create_time = existing_Data.Create_time;
                     _context.Remove(existing_Data);
@@ -153,5 +165,28 @@ namespace InternetAppProject.Controllers
         {
             return _context.User.Any(e => e.Id == id);
         }
+
+        private async void LoginUser(int id, string username)
+        {
+            // HttpContext.Session.SetString("username", username);
+
+            var claims = new List<Claim>
+                {
+                    new Claim(User_Id, id.ToString()),
+                    new Claim(ClaimTypes.Name, username),
+                    //new Claim(ClaimTypes.Role, type.ToString()),
+                };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties();
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+        }
+
     }
 }
