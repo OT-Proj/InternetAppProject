968f63cdf3ebb99b489e0ef468bbd4533b34450b
Or <Or@DESKTOP-495L4VA>
2021-10-10 22:13:09 +0300

    add user annotation validation

--- InternetAppProject/Models/User.cs
+++ InternetAppProject/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,14 +17,25 @@ namespace InternetAppProject.Models
         }
         public int Id { get; set; }
 
+        /* to do: uniqe*/
+
+        [Required(ErrorMessage = "Please enter name"), MaxLength(30)]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public UserType Type { get; set; }
 
+        [DataType(DataType.PostalCode)]
+        [StringLength(8, ErrorMessage = "Zip code contains 7 digits", MinimumLength = 7)]
         public int Zip { get; set; }
-        
+
+        [DataType(DataType.CreditCard)]
+        [StringLength(16, ErrorMessage = "Please enter vaild credit card", MinimumLength = 8)]
         public string Credit_card { get; set; }
 
         public bool Visual_mode { get; set; }
