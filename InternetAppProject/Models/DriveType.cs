1b9e2d0f4ca4a44d0fce0d88a93d24cc51d7ec9d
Or <Or@DESKTOP-495L4VA>
2021-10-11 21:52:02 +0300

    Revert "add DriveTypes annotation"
    This reverts commit 129f6ccbbcdf31045b5256c6a5923bea4aaaf300.

--- InternetAppProject/Models/DriveType.cs
+++ InternetAppProject/Models/DriveType.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,15 +9,10 @@ namespace InternetAppProject.Models
     {
         public int Id { get; set; }
 
-
-        [Required(ErrorMessage = "Please enter name"), MaxLength(30)]
-        [DataType(DataType.Text)]
         public string Name { get; set; }
 
-        /*[RegularExpression(@"^([0-9]{0-3})$", ErrorMessage = "Invalid Capacity")] please check it*/
         public int Max_Capacity { get; set; }
 
-        [DataType(DataType.Currency)]
         public int Price { get; set; }
 
         public DateTime Last_change { get; set; }
