51a044fb62f7155de9d876f5ba097a2631ae167a
Or <Or@DESKTOP-495L4VA>
2021-10-12 23:51:40 +0300

    add video add canvas

---
+++ InternetAppProject/wwwroot/js/canvas.js
@@ -0,0 +1,9 @@
+﻿window.onload = function () {
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.font = "bold 50px Arial";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.strokeStyle = "black";
+    context.strokeText("Welcome!", 150, 40);
+};
