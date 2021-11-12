51caa4fa1795d3e1467da5a415ea6d3fa2db403e
ofek tal <ofek@mail.com>
2021-11-12 09:12:06 +0200

    Minor visual glitch

--- InternetAppProject/wwwroot/js/graph1.js
+++ InternetAppProject/wwwroot/js/graph1.js
@@ -15,7 +15,7 @@
             // Now I can use this dataset:
             function (lineData) {
 
-                var parser = d3.timeParse("%Y-%M-%d")
+                var parser = d3.timeParse("%Y-%m-%d")
 
                 lineData.forEach(function (d) {
                     console.log(d.date);
